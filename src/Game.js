74785ccf83225286f55a7cf0dbfa6b45fbd95583
Pedro S. Moreira <pedsmoreira@gmail.com>
2017-12-10 22:05:44 -0200

    Format with prettier

--- src/Game.js
+++ src/Game.js
@@ -2,8 +2,8 @@ const width = 5;
 const height = 5;
 const direction = "right";
 
-export default class Game{
-  constructor(){
+export default class Game {
+  constructor() {
     this.tail = [];
     this.head = {
       x: Math.floor(width / 2),
@@ -14,29 +14,29 @@ export default class Game{
     this.isDead = false;
   }
 
-  randomFruit(){
+  randomFruit() {
     this.fruit = {
-      x: Math.floor(Math.random() * (width)),
-      y: Math.floor(Math.random() * (height))
+      x: Math.floor(Math.random() * width),
+      y: Math.floor(Math.random() * height)
     };
   }
 
   eat() {
-    let piece = {x: this.head.x, y: this.head.y};
+    let piece = { x: this.head.x, y: this.head.y };
     this.tail.unshift(piece);
     this.randomFruit();
   }
 
   move() {
-    if(this.isDead) return;
+    if (this.isDead) return;
 
     let newX = this.getNewX();
     let newY = this.getNewY();
 
-    if(this.outOfBounds(newX, newY) || this.collide(newX, newY)) {
+    if (this.outOfBounds(newX, newY) || this.collide(newX, newY)) {
       this.die();
     } else {
-      if(this.fruit.x == newX && this.fruit.y == newY) {
+      if (this.fruit.x == newX && this.fruit.y == newY) {
         this.eat();
       } else {
         let piece = this.tail.pop();
@@ -54,30 +54,30 @@ export default class Game{
     return newX > width || newY > height || newX < 0 || newY < 0;
   }
 
-  collide(x, y){
-    this.tail.find((piece) => {
+  collide(x, y) {
+    this.tail.find(piece => {
       return piece.x == x && piece.y == y;
     });
   }
 
-  die(){
+  die() {
     this.isDead = true;
   }
 
-  getNewX(){
-    if(this.direction == "left") return this.head.x-1;
-    if(this.direction == "right") return this.head.x+1;
+  getNewX() {
+    if (this.direction == "left") return this.head.x - 1;
+    if (this.direction == "right") return this.head.x + 1;
     return this.head.x;
   }
 
-  getNewY(){
-    if(this.direction == "up") return this.head.y-1;
-    if(this.direction == "down") return this.head.y+1;
+  getNewY() {
+    if (this.direction == "up") return this.head.y - 1;
+    if (this.direction == "down") return this.head.y + 1;
     return this.head.y;
   }
 
   turn(direction) {
-    if(!this.canTurnTo(direction)) return;
+    if (!this.canTurnTo(direction)) return;
 
     this.direction = direction;
     this.move();
@@ -91,7 +91,7 @@ export default class Game{
     this.turn("right");
   }
 
-  turnUp(){
+  turnUp() {
     this.turn("up");
   }
 
@@ -103,8 +103,10 @@ export default class Game{
     let x = ["left", "right"];
     let y = ["up", "down"];
 
-    if((x.contains(direction) && x.contains(this.direction)) ||
-        y.contains(direction) && y.contains(this.direction)){
+    if (
+      (x.contains(direction) && x.contains(this.direction)) ||
+      (y.contains(direction) && y.contains(this.direction))
+    ) {
       return false;
     }
     return true;
